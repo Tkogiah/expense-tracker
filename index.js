36a644e9d8825df0523285189a82329450b5c2a7
tkogiah <87034470+Tkogiah@users.noreply.github.com>
2021-11-17 13:38:41 -0800

    finally finished main functions

--- index.js
+++ index.js
@@ -3,128 +3,195 @@ function log(a) {
     console.log(a)
 } 
 
+//-------------------- DECLARE MAIN CONSTANTS ---------------------- 
+const objArray = []
+let overSum = [0]
+let overPos = [0]
+let overNeg = [0]
+let total = 0
+let positive = 0
+let negative = 0
 
-
-//-------------------- FIRST - BUILD THE OBJECT ---------------------- 
-
-//OBJECT AND ARRAY TO INDEX OBJECT(S)
-function primaryobject(des, amnt, val, id) {
-    return {
-        'str' : des,
-        'int' : Number(amnt),
-        'value' : val,
-        'id' : Number(id)
-    }
-}
-const objArray = [];
-
-//NODES AND NODE VALUES
+//UI NODES
 const descriptionNode = document.getElementById('description')
 const description = descriptionNode.value
 const amountNode = document.getElementById('amount')
 const amount = amountNode.value
-const positiveNode = document.getElementById('income')
-const positive = positiveNode.value
-const negativeNode = document.getElementById('expense')
-const negative = negativeNode.value
-
-//INITIAL EVENT LISTENERS TO CREATE OBJECTS, PUSH TO ARRAY, ASSIGN ID, DISPLAY CONTENT
-positiveNode.addEventListener('click', function() {
-    if(!description || !amount) return;
-    let incomeObject = primaryobject(description, amount, positive);    
-    objArray.push(incomeObject)
-    assignId(objArray)
-    sumFunctions(objArray)
-    mainDisplay(objArray)
-
-})
-negativeNode.addEventListener('click', function() {
-    if(!description || !amount) return;
-    let incomeObject = primaryobject(description, amount, negative);
-    incomeObject.int = -Math.abs(amount)    
-    objArray.push(incomeObject)
-    assignId(objArray)
-    sumFunctions(objArray)
-    mainDisplay(objArray)
-
-})
-
-//ASSIGN ID TO ARRAY, SHOULD BE CALLED ON EVERY BUTTON
-function assignId(array) {
-    array.forEach((item, i) => {
-        item.id = i;
-      });
-    
-}
-
-
-
-
-//-------------------- SECOND - BUILD FUNCTIONS TO DISPLAY OBJECT(S) CONTENT ---------------------- 
+const incomeNode = document.getElementById('income-btn')
+const incomeClass = incomeNode.value
+const expenseNode = document.getElementById('expense-btn')
+const expenseClass = expenseNode.value
 //OVERVIEW NODES
 const balanceOverview = document.getElementById('overview-balance')
 const incomeOverview = document.getElementById('overview-income')
 const expenseOverview = document.getElementById('overview-expenses')
 //TAB NODES
-const allTab = document.getElementById('tabs-all')
-const incomeTab = document.getElementById('tabs-income')
-const expensesTab = document.getElementById('tabs-expenses')
-//DISPLAY NODE
-const display = document.getElementById('display')
-
-//DISPLAY FUNCTION
-function mainDisplay(obj) {
-
-    
-    
-    let end = obj.length-1
-    let dashboardItem = `
-        <div class='dashboard'>
-            <li class='${obj[end].value}' id='${obj[end].id}'>
-                <div class='dashboard-item'>
-                    ${obj[end].str} : $ ${obj[end].int}
-                </div>
-                <div class='dashboard-edit-delete'>
-                    <input name='edit' id='edit' type='image' src='https://img.icons8.com/wired/50/000000/edit.png'>    
-                    <input name='delete' id='delete' type='image' src='https://img.icons8.com/external-wanicon-lineal-wanicon/64/000000/external-delete-user-interface-wanicon-lineal-wanicon.png'>
-                </div>
-            </li>
-        </div>    
-    `
-    display.insertAdjacentHTML('afterbegin', dashboardItem)
+const allTab = document.querySelector('.tabs-all')
+const incomeTab = document.querySelector('.tabs-income')
+const expensesTab = document.querySelector('.tabs-expenses')
+//DISPLAY NODES
+const display = document.querySelector('.show')
 
 
-}
 
-//expenseOverview incomeOverview balanceOverview
-function sumFunctions(obj) {
-    let total = 0;
-    let pos = 0;
-    let neg = 0;
-    if(obj.length < 2) return
-        
-    for(let i = 0; i < obj.length; i++) {
-        if(obj[i]['int'] < 0) {
-            neg = neg + obj[i]['int']
-        }
-        else if(obj[i]['int'] > 0) {
-            pos = pos + obj[i]['int']
-        }
-        total = total + obj[i]['int']
+
+//-------------------------- BUILD FUNCTIONS -------------------------
+
+//OBJECT AND ARRAY TO INDEX OBJECT(S)
+function primaryObject(des, amnt, val, id) {
+    return {
+        'str' : des,
+        'int' : Number(amnt),
+        'class' : val,
+        'id' : id
     }
-
-    balanceOverview.innerHTML = `$ ${total}`
-    incomeOverview.innerHTML = `$ ${pos}`
-    expenseOverview.innerHTML = `$ ${neg}`
-
 }
 
-function toggleTab()
+//ADD NEW ITEM EVENT LISTENER
+function incomeOrExpense(node) {
+    node.addEventListener('click', function() {
+        let overSum = [0]
+        let overPos = [0]
+        let overNeg = [0]
+        let total = 0
+        let positive = 0
+        let negative = 0
+        if(!descriptionNode.value || !amountNode.value) return;
+        let object = primaryObject(descriptionNode.value, amountNode.value, node.value)
+        document.getElementById('description').value = ''
+        document.getElementById('amount').value = ''
+        objArray.push(object)
+        overviewArray(objArray, overSum, overPos, overNeg)
+        total = overSum.reduce((a,b) => { return a + b })  
+        positive = overPos.reduce((a,b) => { return a + b })
+        negative = overNeg.reduce((a,b) => { return a + b })  
+        updateUI(objArray, total, positive, negative)
+    })
+}
+
+//BUILD OVERVIEW ARRAYS
+function overviewArray(arr, arr1, arr2, arr3) {
+    arr.forEach(element => {
+        if (element.class == 'income') {
+            arr1.push(Number(element.int))
+            arr2.push(Number(element.int))
+
+        }
+        else if (element.class == 'expense') {
+            arr1.push(-Math.abs(element.int))
+            arr3.push(-Math.abs(element.int))
+        }
+    })
+        
+}
+
+//BUILD LI FROM ARRAY
+function arrayToLi(array) {
+    array.forEach((element, index) => {
+        element.id = index;
+        let dashboardItem = `
+            <div class='${element.class} li-items'id='${element.id}'>
+                <li id='${element.id}'>
+                    <div class='dashboard-item'>
+                        ${element.str} : $ ${element.int}
+                    </div>
+                    <div class='delete'>
+                        <input class='delete' name='delete' id='delete' type='image' src='https://img.icons8.com/external-wanicon-lineal-wanicon/64/000000/external-delete-user-interface-wanicon-lineal-wanicon.png'>
+                    </div>
+                </li>
+            </div>    
+        `
+        display.insertAdjacentHTML('afterbegin', dashboardItem)
+        const liDelete = document.getElementById('delete')
+        liDelete.addEventListener('dblclick', deleteItem)
+    });
+}
 
 
-// - write function to delete said object from an array
 
-// - write function to edit said object from an array
+//-------------------------- DISPLAY FUNCTIONS -------------------------
 
-// - write function to pull object data and sum it
+//UPDATE UI
+function updateUI(array, num1, num2, num3) {
+    assignOverviewNode(balanceOverview, num1)
+    assignOverviewNode(incomeOverview, num2)
+    assignOverviewNode(expenseOverview, num3)
+    //remove all li from display
+    document.querySelectorAll('.li-items').forEach((element) => element.remove());    
+    //add all li to display
+    arrayToLi(array)
+    description = ''
+    amount = ''
+}
 
+//ASSIGN OVERVIEW NODES
+function assignOverviewNode(node, num) {
+    
+    node.innerHTML = num
+}
+
+
+//-------------------------- TOGGLE FUNCTIONS -------------------------
+
+allTab.addEventListener('click', function() {
+    allTab.classList.remove('inactive')
+    incomeTab.classList.add('inactive')
+    expensesTab.classList.add('inactive')
+    let displayNodes = document.querySelectorAll('.li-items')
+    toggleLi(displayNodes)
+})
+incomeTab.addEventListener('click', function() {
+    incomeTab.classList.remove('inactive')
+    allTab.classList.add('inactive')
+    expensesTab.classList.add('inactive')
+    let displayNodes = document.querySelectorAll('.li-items')
+    let expenseNodes = document.querySelectorAll('.expense')
+    toggleLi(displayNodes)
+    addHide(expenseNodes)
+})
+expensesTab.addEventListener('click', function() {
+    expensesTab.classList.remove('inactive')
+    allTab.classList.add('inactive')
+    incomeTab.classList.add('inactive')
+    let displayNodes = document.querySelectorAll('.li-items')
+    let incomeNodes = document.querySelectorAll('.income')
+    toggleLi(displayNodes)
+    addHide(incomeNodes)
+})
+
+function toggleLi(nodes) {
+    for (let node of nodes) {   
+        node.classList.remove('hide');
+    }
+}
+function addHide(nodes) {
+    for (let node of nodes) {   
+        node.classList.add('hide');
+    }
+}
+
+
+//-------------------------- DELETE FUNCTION -------------------------
+
+function deleteItem(event){
+    const targetBtn = event.target;
+    const entry = targetBtn.parentNode.parentNode.parentNode;
+    objArray.splice( entry.id, 1);
+    let overSum = [0]
+    let overPos = [0]
+    let overNeg = [0]
+    let total = 0
+    let positive = 0
+    let negative = 0
+    overviewArray(objArray, overSum, overPos, overNeg)
+    total = overSum.reduce((a,b) => { return a + b })  
+    positive = overPos.reduce((a,b) => { return a + b })
+    negative = overNeg.reduce((a,b) => { return a + b })  
+    updateUI(objArray, total, positive, negative)
+}
+
+
+//-------------------------- FUNCTION CALLS -------------------------
+
+incomeOrExpense(incomeNode)
+incomeOrExpense(expenseNode)
